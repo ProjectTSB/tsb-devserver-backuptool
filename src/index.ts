6492a120aa937738a00d1c40a9e9bf8743a00bc6
MT224244 <mt224244@gmail.com>
2020-08-16 20:25:22 +0900

    RconでMinecraftサーバーにコマンドを送信する処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import yaml from 'js-yaml';
 import { readFileSync, existsSync } from 'fs';
 import path from 'path';
+import { Rcon } from 'rcon-client';
 
 import { Config } from './types/Config';
 import { DriveUpload } from './util/DriveUpload';
@@ -8,19 +9,43 @@ import { Compress } from './util/Compress';
 import { Pipe } from './util/Pipe';
 import { FileName } from './util/FileName';
 
-const conf: Config = yaml.load(readFileSync('config.yaml', 'utf-8'));
+const main = async () => {
+    const conf: Config = yaml.load(readFileSync('config.yaml', 'utf-8'));
 
-if (!existsSync(conf.serverDirPath)) {
-    console.error('サーバーディレクトリが見つかりません');
-    process.exit(1);
-}
+    try {
+        const rcon = await Rcon.connect({
+            host: conf.rcon.host,
+            port: conf.rcon.port,
+            password: conf.rcon.password
+        });
 
-for (const dirName of conf.worldDirNames) {
-    if (!existsSync(path.join(conf.serverDirPath, dirName))) {
-        console.error('ワールドディレクトリが見つかりません');
+        if (!existsSync(conf.serverDirPath)) {
+            console.error('サーバーディレクトリが見つかりません');
+            process.exit(1);
+        }
+
+        for (const dirName of conf.worldDirNames) {
+            if (!existsSync(path.join(conf.serverDirPath, dirName))) {
+                console.error('ワールドディレクトリが見つかりません');
+                process.exit(1);
+            }
+        }
+
+        await rcon.send('save-all');
+        await rcon.send('save-off');
+        await rcon.send('tellraw @a {"text": "ワールドをバックアップ中...", "bold": true, "color": "blue"}');
+
+        const stream = Compress(conf.serverDirPath, conf.worldDirNames);
+        await DriveUpload(stream.pipe(new Pipe()), conf.driveDirId, FileName());
+
+        await rcon.send('tellraw @a {"text": "バックアップが完了しました", "bold": true, "color": "blue"}');
+        await rcon.send('save-on');
+
+        await rcon.end();
+    }
+    catch {
+        console.error('サーバーに接続できませんでした');
         process.exit(1);
     }
-}
-
-const stream = Compress(conf.serverDirPath, conf.worldDirNames);
-DriveUpload(stream.pipe(new Pipe()), conf.driveDirId, FileName());
+};
+main();
--- src/types/Config.ts
+++ src/types/Config.ts
@@ -2,4 +2,9 @@ export type Config = {
     serverDirPath: string;
     worldDirNames: string[];
     driveDirId: string;
+    rcon: {
+        host: string;
+        port: number;
+        password: string;
+    };
 };
